99631f9e88352b25ba15376a2cbd95f9463b445e
dharmendrasingh2020 <67416349+dharmendrasingh2020@users.noreply.github.com>
2020-07-02 10:09:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+( function(){
+    'use strict';
+    angular.module('myFirstApp',[])
+    .controller('MyFirstAppController', function ($scope)
+    {
+
+       $scope.name="";
+       $scope.totalValue= 0;
+       $scope.displayNemeric=funtion ()
+       {
+         var toatlNameValue = calNValueString;
+         $scope.totalValue = toatlNameValue;
+
+       };
+
+
+       
+
+      
+        
+    });
+
+})();
+ 
