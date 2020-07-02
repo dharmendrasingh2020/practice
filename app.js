09295f0c0f36d8da8e223f91e4cdb4a7b28a81bf
dharmendrasingh2020 <67416349+dharmendrasingh2020@users.noreply.github.com>
2020-07-02 16:09:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 ( function(){
-    'use strict';
-    angular.module('myFirstApp',[])
-
-
-    .controller('MyFirstAppController', function ($scope)
-    {
-
-       $scope.name="";
-       $scope.totalValue= 0;
-
-
-       $scope.displayNemeric=funtion()
-       {
-         var toatlNameValue =calculateNeumericString($scope.name)
-         $scope.totalValue = toatlNameValue;
-
-       };
-
-       funtion calculateNeumericString(string){
-        var toatlStringValue =0;
-         toatlStringValue +=string.charCodeAt(i);
-       }   
-     return toatlStringValue;
-        
-    });
+  'use strict';
+  angular.module('myFirstApp', [])
+  .controller('MyFirstAppController', function ($scope)
+  {
+     $scope.name="";
+     $scope.totalValue= 0;
+     $scope.displayNemeric = funtion ()
+     {
+       var totalNameValue = calculateNeumericString($scope.name);
+       $scope.totalValue = totalNameValue;
+     };
+     function calculateNeumericString(string)
+     {
+      var totalNameValue =0;
+       toatlStringValue +=string.charCodeAt(i);
+     }   
+   return toatlStringValue;
+      
+  });
 
 })();
- 
