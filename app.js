01b1d24222c8f6c8df63603b829a84abe9635dd9
dharmendrasingh2020 <67416349+dharmendrasingh2020@users.noreply.github.com>
2020-07-02 11:51:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 ( function(){
     'use strict';
     angular.module('myFirstApp',[])
+
+    
+
     .controller('MyFirstAppController', function ($scope)
     {
 
        $scope.name="";
        $scope.totalValue= 0;
+
+
+       $scope.displayNemeric=funtion()
+       {
+         var toatlNameValue =0;
+         $scope.totalValue = toatlNameValue;
+
+       };
+
+       funtion calculateNeumericString(string){
+        var toatlStringValue =0;
+         toatlStringValue +=string.charCodeAt(i);
+       }   
        
-          
+      
         
     });
 
